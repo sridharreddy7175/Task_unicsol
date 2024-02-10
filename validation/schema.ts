266d7880d6005c034233c71ef3af343c5ff6aec5
sridharreddy-mrsoft <psridharreddy@mrsoftsolutions.com>
2024-02-10 14:58:22 +0530

    Added Task

--- validation/schema.ts
+++ validation/schema.ts
@@ -2,17 +2,17 @@ import Joi from 'joi';
 
 export const userRegistrationSchema = Joi.object({
     name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    email: Joi.string().trim().regex(/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,20}$/).required(),
+    password: Joi.string().trim().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])(?!.*\s).{8,}$/).required(),
 });
 
 export const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    email: Joi.string().trim().regex(/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,20}$/).required(),
+    password: Joi.string().trim().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])(?!.*\s).{8,}$/).required(),
 });
 
 export const BookSchema=Joi.object({
     name: Joi.string().alphanum().min(3).max(30).required(),
-    description: Joi.string().alphanum().min(3).max(30).required(),
+    description: Joi.string().required(),
 })
 
